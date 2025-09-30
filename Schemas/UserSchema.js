e97b7357b516fae53756df8754bf38e6de9e4da9
WarriorSFG <samarthgupta9999@gmail.com>
2025-09-30 13:54:10 +0530

    Update UserSchema.js

--- Schemas/UserSchema.js
+++ Schemas/UserSchema.js
@@ -1,4 +1,23 @@
 import mongoose from "mongoose";
+
+const getRandomGameryAvatar = () => {
+    // A list of cool, gamery avatar styles from DiceBear
+    const avatarStyles = [
+        'pixel-art-neutral',
+        'adventurer',
+        'bottts'
+    ];
+
+    // 1. Randomly select one of the styles from the list
+    const style = avatarStyles[Math.floor(Math.random() * avatarStyles.length)];
+    
+    // 2. Generate a random string (seed) to make each avatar unique
+    const seed = Math.random().toString(36).substring(2, 12);
+
+    // 3. Construct and return the final URL
+    return `https://api.dicebear.com/8.x/${style}/svg?seed=${seed}`;
+};
+
 export const UserSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -23,7 +42,7 @@ export const UserSchema = new mongoose.Schema({
     },    
     avatar: {
         type: String,
-        default: () => `https://i.pravatar.cc/150?img=${Math.floor(Math.random() * 70)}`
+        default: getRandomGameryAvatar()
     },
         likedGames: {
         type: [String], 
